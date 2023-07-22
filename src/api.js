80e2fe5f7d4de6db597b90272d308f9b2e219510
Mark3vich <mango.2004@yandex.ru>
2023-07-22 19:40:25 +0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = ''; // Ведите свой ключ сюда
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '', // Ведите свой ключ сюда
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
